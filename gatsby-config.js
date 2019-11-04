06def1e3f025194d20a0995b197a205a0e59533f
rickywep <ricky.gnr.moreno@gmail.com>
2019-11-04 10:17:18 -0300

    primer commit antes del despliegue

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "http://localhost:1337",
+        contentTypes: [
+          // Lista de tipos de contenido que tenemos en Strapi, en singular
+          "articulo"
+        ],
+        queryLimit: 1000,
+      },
+     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
+ 
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <ul>
+      {data.allStrapiArticulo.edges.map(({ node }) => (
+    <li key={node.strapiId}>
+      <h2>
+        <Link to={`/${node.strapiId}`}>{node.titulo}</Link>
+      </h2>
+      <p>{node.descripcion}</p>
+    </li>
+))}
+  </ul>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
-
+ 
 export default IndexPage
+ 
+export const pageQuery = graphql`
+  query IndexQuery {
+    allStrapiArticulo {
+      edges {
+        node {
+          strapiId
+          titulo
+          descripcion
+        }
+      }
+    }
+  }
+`
